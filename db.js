b834c41dd89015227e3420d7b9b9b8583111102c
Michel Bernardo <bernardoenglish@outlook.com>
2020-04-01 12:28:21 -0400

    banco de dados $

---
+++ db.js
@@ -0,0 +1,59 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./CC.db');
+
+db.serialize(function () {
+
+    // CRIAR TABELA SE ELA NÃO EXISTIR
+    db.run(`
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            title TEXT,
+            category TEXT,
+            description TEXT,
+            link TEXT
+        );
+    `)
+
+    // const query = `
+    // INSERT INTO ideas(
+    //     image,
+    //     title, 
+    //     category,
+    //     description,
+    //     link
+    // ) VALUES (?,?,?,?,?);
+    // `
+
+    // const values = [
+    //     "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+    //     "Cursos de Programação",
+    //     "Estudo",
+    //     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima reprehenderit tempora",
+    //     "http://www.rocketseat.com.br"
+    // ]
+
+    // //INSERIR DADOS NA TABELA
+    // db.run(query, values, function (err) {
+    //     if (err) return console.log(err)
+    
+    //     console.log(this)
+    // });
+
+    //CONSULTAR DADOS DA TABELA
+    // db.all(`SELECT * FROM ideas `, function(err, rows) {
+    //     if (err) return console.log(err)
+
+    //     console.log(rows)
+    // });
+
+    // // DELETAR DADOS DA TEBELA
+    // db.run(`DELETE FROM ideas WHERE id =?`, [1], function(err) {
+    //      if (err) return console.log(err)
+
+    //      console.log("DELETEI", this)
+    // });
+
+});
+
+exports.modules = db
--- server.js
+++ server.js
@@ -2,40 +2,39 @@
 const express = require('express');
 const server = express();
 
+// const ideas = [
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+//             title: "Cursos de Programação",
+//                 category: "Estudo",
+//                     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima reprehenderit tempora",
+//                         url: "http://www.rocketseat.com.br"
+//     },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+//             title: "Exercícios",
+//                 category: "Saúde",
+//                     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima reprehenderit tempora",
+//                         url: "http://www.rocketseat.com.br"
+//     },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+//             title: "Meditação",
+//                 category: "Saúde",
+//                     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima reprehenderit tempora",
+//                         url: "http://www.rocketseat.com.br"
+//     },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+//             title: "Karakokê",
+//                 category: "Diversão",
+//                     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima reprehenderit tempora",
+//                         url: "http://www.rocketseat.com.br"
+//     }
+// ]
+
+
 server.use(express.static("public"));
-
-const ideas = [
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-            title: "Cursos de Programação",
-                category: "Estudo",
-                    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima reprehenderit tempora",
-                        url: "http://www.rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-            title: "Exercícios",
-                category: "Saúde",
-                    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima reprehenderit tempora",
-                        url: "http://www.rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-            title: "Meditação",
-                category: "Saúde",
-                    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima reprehenderit tempora",
-                        url: "http://www.rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-            title: "Karakokê",
-                category: "Diversão",
-                    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima reprehenderit tempora",
-                        url: "http://www.rocketseat.com.br"
-    }
-]
-
-
 
 // configuração do nunjucks
 const nunjucks = require('nunjucks');
@@ -44,26 +43,40 @@ nunjucks.configure("views", {
     noCache: true,
 })
 
-//Criação de Rota
+
+//Rendeniza a página inicial
 server.get("/", function (request, response) {
 
-    const reversedIdeas = [...ideas].reverse()
-    
-    const lastIdeas = []
-    for (let idea of reversedIdeas) {
-        if(lastIdeas.length < 2) {
-            lastIdeas.push(idea)
+    db.all(`SELECT * FROM ideas`, function (err, rows) {
+        if (err) return console.log(err)
+
+        console.log(rows)
+
+        const reversedIdeas = [...rows].reverse()
+
+        let lastIdeas = []
+        for (let idea of reversedIdeas) {
+            if (lastIdeas.length < 2) {
+                lastIdeas.push(idea)
+            }
         }
-    }
 
-    return response.render("index.html", { ideas: lastIdeas})
-});
+        return response.render("index.html", { ideas: lastIdeas })
+    })
+})
 
+
+//Rendeniza a página de ideias
 server.get("/ideas", function (request, response) {
 
-    const reversedIdeas = [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function (err, rows) {
+        if (err) return console.log(err)
 
-    return response.render("ideas.html", { ideas: reversedIdeas})
+        const reversedIdeas = [...rows].reverse()
+        return response.render("ideas.html", { ideas: reversedIdeas })
+
+    })
+
 });
 
 server.listen(3000);
